a4cfa23459fe443349c2b4a1df0642d337b1a83a
TanelP <tanel.pool@tktk.ee>
2024-12-20 15:38:51 +0200

    add week 8 code

---
+++ Week8EpicGame/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.CompilerServices;
+
+string folderPath = @"C:\data";
+string heroFile = "heroes.txt";
+string villainFile = "villains.txt";
+
+string[] heroes = File.ReadAllLines(Path.Combine(folderPath,heroFile));
+string[] villains = File.ReadAllLines(Path.Combine(folderPath, villainFile));
+
+
+//string[] heroes = { "Batman", "Ironman", "Johnny Bravo", "Robocop" };
+//string[] villains = { "Käsnakalle", "Teletups", "Lotte", "Muumipapa", "Elmo" };
+
+
+
+string[] weapon = { "hundinui", "kirvevars", "käekott", "padi", "porgand" };
+
+//Random rnd = new Random();
+//int randomIndex = rnd.Next(0, heroes.Length);
+
+string hero = GetRandomValueFromArray(heroes);
+string heroWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {hero} wielding {heroWeapon} saves the day!");
+
+string villain = GetRandomValueFromArray(villains);
+string villainWeapon = GetRandomValueFromArray(weapon);
+Console.WriteLine($"Today {villain} wielding {villainWeapon} tries to take over the world");
+
+
+static string GetRandomValueFromArray(string[] someArray)
+{
+    Random rnd = new Random();
+    int randomIndex = rnd.Next(0, someArray.Length);
+    string randomStringFromArray = someArray[randomIndex];
+    return randomStringFromArray;
+}
---
+++ Week8Test/Program.cs
@@ -0,0 +1,65 @@
+﻿//string path = @"C:\data";
+//string fileName = "chuck.txt";
+
+//string[] lines = File.ReadAllLines(Path.Combine(path, fileName));
+
+//foreach (string hero in lines)
+//{
+//    Console.WriteLine(hero);
+//}
+
+//1 - ok
+//string path = @"C:\data";
+//string fileName = "chuck.txt";
+//string[] lines = File.ReadAllLines(Path.Combine(path, fileName));
+//Console.WriteLine(lines.Length);
+
+//2 - vale
+//string path = @"C:\data";
+//string fileName = "chuck.txt";
+//string[] lines = File.ReadAllLines($"{path} + {fileName}");
+
+//foreach (string hero in lines)
+//{
+//    Console.WriteLine(hero);
+//}
+
+//3 - vale
+//string path = @"C:\data";
+//string fileName = "chuck.txt";
+//string[] lines = File.ReadAllLines(path + fileName);
+
+//foreach (string hero in lines)
+//{
+//    Console.WriteLine(hero);
+//}
+
+//4 - õige
+//string path = @"C:\data\";
+//string fileName = "chuck.txt";
+//string[] lines = File.ReadAllLines(path + fileName);
+
+//foreach (string hero in lines)
+//{
+//    Console.WriteLine(hero);
+//}
+
+
+
+
+//5
+string path = @"C:\data";
+string fileName = "chuck.txt";
+string[] lines = File.ReadAllLines(Path.Combine(path, fileName));
+
+ShowFileContent(lines);
+
+static void ShowFileContent(string[] fileContentArray)
+{
+    int i = new int();
+    foreach (string line in fileContentArray)
+    {
+        Console.WriteLine($"{i}. {fileName}");
+        i++;
+    }
+}
